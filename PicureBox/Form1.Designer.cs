4b120adb6d1128ed19228d890b58eda2f857de0d
Piotr Wardzyński <pwardzynski@transroad.com.pl>
2022-01-27 14:41:21 +0100

    Dodaj pliki projektów.

---
+++ PicureBox/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PicureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picbPhoto = new System.Windows.Forms.PictureBox();
+            this.btnAddPhoto = new System.Windows.Forms.Button();
+            this.btnDeletePhoto = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lbFile = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picbPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picbPhoto
+            // 
+            this.picbPhoto.AccessibleDescription = "Moje zdjęcie";
+            this.picbPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picbPhoto.BackColor = System.Drawing.SystemColors.Control;
+            this.picbPhoto.Location = new System.Drawing.Point(12, 88);
+            this.picbPhoto.Name = "picbPhoto";
+            this.picbPhoto.Size = new System.Drawing.Size(436, 257);
+            this.picbPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbPhoto.TabIndex = 0;
+            this.picbPhoto.TabStop = false;
+            this.picbPhoto.Visible = false;
+            // 
+            // btnAddPhoto
+            // 
+            this.btnAddPhoto.Location = new System.Drawing.Point(12, 12);
+            this.btnAddPhoto.Name = "btnAddPhoto";
+            this.btnAddPhoto.Size = new System.Drawing.Size(98, 23);
+            this.btnAddPhoto.TabIndex = 1;
+            this.btnAddPhoto.Text = "Wczytaj zdjęcie";
+            this.btnAddPhoto.UseVisualStyleBackColor = true;
+            this.btnAddPhoto.Click += new System.EventHandler(this.btnAddPhoto_Click);
+            // 
+            // btnDeletePhoto
+            // 
+            this.btnDeletePhoto.Enabled = false;
+            this.btnDeletePhoto.Location = new System.Drawing.Point(163, 12);
+            this.btnDeletePhoto.Name = "btnDeletePhoto";
+            this.btnDeletePhoto.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletePhoto.TabIndex = 2;
+            this.btnDeletePhoto.Text = "Usuń zdjęcie";
+            this.btnDeletePhoto.UseVisualStyleBackColor = true;
+            this.btnDeletePhoto.Click += new System.EventHandler(this.btnDeletePhoto_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Enabled = false;
+            this.btnPreview.Location = new System.Drawing.Point(12, 41);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(98, 23);
+            this.btnPreview.TabIndex = 3;
+            this.btnPreview.Text = "<< Poprzednie <<";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(163, 40);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(98, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = ">> Następne >>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lbFile
+            // 
+            this.lbFile.AutoSize = true;
+            this.lbFile.Location = new System.Drawing.Point(12, 67);
+            this.lbFile.Name = "lbFile";
+            this.lbFile.Size = new System.Drawing.Size(0, 13);
+            this.lbFile.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 355);
+            this.Controls.Add(this.lbFile);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnDeletePhoto);
+            this.Controls.Add(this.btnAddPhoto);
+            this.Controls.Add(this.picbPhoto);
+            this.Name = "Form1";
+            this.Text = "Przeglądarka zdjęć";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.picbPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picbPhoto;
+        private System.Windows.Forms.Button btnAddPhoto;
+        private System.Windows.Forms.Button btnDeletePhoto;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lbFile;
+    }
+}
+
---
+++ PicureBox/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PicureBox.Properties;
+
+namespace PicureBox
+{
+    public partial class Form1 : Form
+    {
+        private enum order {Poprzedni, Następny};
+        private List<string> _listFile;
+        private int _curFileNumber, _lasFileNumber;
+        private string _fileExtOK;
+        private string _picture
+        {
+            get
+            {
+                return Settings.Default.DefaultPicture;
+            }
+            set
+            {
+                Settings.Default.DefaultPicture = value;
+            }
+        }
+        private string _folder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _fileExtOK = "JPG, JPEG, BMP, GIF";
+            if (_picture.Length > 0)
+            {
+                ViewPhoto(_picture);
+                GetFileFolderFromFile(_picture);
+                GetFileTable(_picture);
+                SetBtnPrevNextEnable();
+            }
+        }
+
+        private void btnAddPhoto_Click(object sender, EventArgs e)
+        {
+            var path = GetFileFolder();
+            if (path.Length > 0)
+            {
+                ViewPhoto(path);
+                GetFileFolderFromFile(path);
+                GetFileTable(path);
+                SetBtnPrevNextEnable();
+            }
+        }
+
+        private string GetFileFolder()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Pliki graficzne *.jpg|*.jpg| *.jpeg|*.jpeg| *bmp|*bmp| *gif|*gif";
+            fileDialog.Title = "Wybierz zdjęcie do załadowania";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+                return fileDialog.FileName;
+            return "";
+        }
+
+        private void ViewPhoto(string path)
+        {
+            picbPhoto.ImageLocation = path;
+            btnDeletePhoto.Enabled = true;
+            lbFile.Text = path;
+            picbPhoto.Visible = true;
+        }
+
+        private void GetFileFolderFromFile(string path)
+        {
+            _folder = Path.GetDirectoryName(path);
+        }
+
+        private void btnDeletePhoto_Click(object sender, EventArgs e)
+        {
+            picbPhoto.ImageLocation = "";
+            btnDeletePhoto.Enabled = false;
+            btnNext.Enabled = false;
+            btnPreview.Enabled = false;
+            lbFile.Text = "";
+            ClearVariableLocal();
+        }
+
+        private void ClearVariableLocal()
+        {
+            _folder = "";
+            _listFile = null;
+            _curFileNumber = 0;
+            _lasFileNumber = 0;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _picture = picbPhoto.ImageLocation;
+            Settings.Default.Save();
+        }
+
+        private void GetFileTable(string fileFolder)
+        {
+            _listFile = Directory.GetFiles(_folder).ToList();
+            _listFile = Directory.GetFiles(_folder).ToList().Where(s => _fileExtOK.Contains(s.Split('.').Last().ToUpper())).ToList();
+            _curFileNumber = _listFile.Select((s, index) => new { s, index }).Where(x => x.s == fileFolder).Select(x => x.index).FirstOrDefault();
+            _lasFileNumber = _listFile.Count - 1;
+        }
+
+        private void SetBtnPrevNextEnable()
+        {
+            btnPreview.Enabled = true;
+            if (_curFileNumber == 0)
+                btnPreview.Enabled = false;
+            btnNext.Enabled = true;
+            if (_curFileNumber == _lasFileNumber)
+                btnNext.Enabled = false;
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            GetNextPreviewPhoto(order.Poprzedni);
+            SetBtnPrevNextEnable();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            GetNextPreviewPhoto(order.Następny);
+            SetBtnPrevNextEnable();
+        }
+
+        private void GetNextPreviewPhoto(order order)
+        {   //Po tym jak ograniczyłem listę plików w tablicy, po zaczytaniu folderu, do danych .ext można ograniczyć tą metodę.
+            string file;
+            int i = 1;
+            while (true)
+            {
+                if (order == order.Poprzedni)
+                {
+                    file = _listFile[_curFileNumber - i];
+                    _curFileNumber--;
+                }
+                else
+                {
+                    file = _listFile[_curFileNumber + i];
+                    _curFileNumber++;
+                }
+                var fileExt = file.Split('.').Last().ToUpper();
+                if (_fileExtOK.Contains(fileExt))
+                    break;
+                if (_curFileNumber == 0 || _curFileNumber == _lasFileNumber)
+                {
+                    var btnInfo = "koniec";
+                    if (order == order.Poprzedni)
+                        btnInfo = "początek";
+                    MessageBox.Show($"Brak pliku do wyświetlenia\nOsiągnięto {btnInfo} folderu");
+                    return;
+                }
+            }
+            ViewPhoto(file);
+        }
+    }
+}
